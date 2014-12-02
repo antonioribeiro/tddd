2a53f2e02c07e688d10c2c582ceb002bfc4d6025
Mark Ewers <mark.ewers@gmail.com>
2014-12-02 16:41:04 -0600

    refactored repo method calls into repo

--- src/Services/Watcher.php
+++ src/Services/Watcher.php
@@ -2,318 +2,298 @@
 
 namespace PragmaRX\Ci\Services;
 
-use PragmaRX\Ci\Data\Repositories\Data as DataRepository;
-use Config;
 use App;
+use Config;
 use Illuminate\Console\Command;
 use JasonLewis\ResourceWatcher\Event;
-use PragmaRX\Ci\Vendor\Laravel\Entities\Project;
-use PragmaRX\Ci\Vendor\Laravel\Entities\Suite;
+use PragmaRX\Ci\Data\Repositories\Data as DataRepository;
 
-class Watcher {
+class Watcher
+{
 
-	/**
-	 * Is the watcher initialized?
-	 *
-	 * @var
-	 */
-	protected $is_initialized;
+    /**
+     * Is the watcher initialized?
+     *
+     * @var
+     */
+    protected $is_initialized;
 
-	/**
-	 * Folders to be watched.
-	 *
-	 * @var
-	 */
-	protected $watchFolders;
+    /**
+     * Folders to be watched.
+     *
+     * @var
+     */
+    protected $watchFolders;
 
-	/**
-	 * The file watcher.
-	 *
-	 * @var
-	 */
-	protected $watcher;
+    /**
+     * The file watcher.
+     *
+     * @var
+     */
+    protected $watcher;
 
-	/**
-	 * Folder listeners.
-	 *
-	 * @var
-	 */
-	protected $listeners;
+    /**
+     * Folder listeners.
+     *
+     * @var
+     */
+    protected $listeners;
 
-	/**
-	 * Console command object.
-	 *
-	 * @var
-	 */
-	protected $command;
+    /**
+     * Console command object.
+     *
+     * @var
+     */
+    protected $command;
 
-	/**
-	 * Exclude folders.
-	 *
-	 * @var
-	 */
-	protected $exclusions;
+    /**
+     * Exclude folders.
+     *
+     * @var
+     */
+    protected $exclusions;
 
-	/**
-	 * Watcher Repository.
-	 *
-	 * @var DataRepository
-	 */
-	private $dataRepository;
+    /**
+     * Watcher Repository.
+     *
+     * @var DataRepository
+     */
+    private $dataRepository;
 
-	/**
-	 * Instantiate a Watcher.
-	 *
-	 * @param DataRepository $dataRepository
-	 */
-	public function __construct(DataRepository $dataRepository)
-	{
-		$this->dataRepository = $dataRepository;
+    /**
+     * Instantiate a Watcher.
+     *
+     * @param DataRepository $dataRepository
+     */
+    public function __construct(DataRepository $dataRepository)
+    {
+        $this->dataRepository = $dataRepository;
 
-		$this->watcher = App::make('watcher');
-	}
+        $this->watcher = App::make('watcher');
+    }
 
-	/**
-	 * Watch for file changes.
-	 *
-	 * @param Command $command
-	 * @return bool
-	 */
-	public function run(Command $command)
-	{
-		$this->command = $command;
+    /**
+     * Watch for file changes.
+     *
+     * @param Command $command
+     *
+     * @return bool
+     */
+    public function run(Command $command)
+    {
+        $this->command = $command;
 
-		$this->initialize();
+        $this->initialize();
 
-		$this->watch();
+        $this->watch();
 
-	    return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Initialize the Watcher.
-	 *
-	 */
-	private function initialize()
-	{
-		$this->command->comment('Laravel-CI - Watcher');
+    /**
+     * Initialize the Watcher.
 
-		if ( ! $this->is_initialized)
-		{
-			$this->loadEverything();
+     */
+    private function initialize()
+    {
+        $this->command->comment('Laravel-CI - Watcher');
 
-			$this->is_initialized = true;
-		}
-	}
+        if ( ! $this->is_initialized) {
+            $this->loadEverything();
 
-	/**
-	 * Read configuration and load testers, projects, suites...
-	 *
-	 */
-	private function loadEverything()
-	{
-		$this->command->line('Loading testers...');
-		$this->loadTesters();
+            $this->is_initialized = true;
+        }
+    }
 
-		$this->command->line('Loading projects and suites...');
-		$this->loadProjects();
+    /**
+     * Read configuration and load testers, projects, suites...
 
-		$this->command->line('Loading tests...');
-		$this->loadTests();
-	}
+     */
+    private function loadEverything()
+    {
+        $this->command->line('Loading testers...');
+        $this->loadTesters();
 
-	/**
-	 * Load all testers to database.
-	 *
-	 */
-	private function loadTesters()
-	{
-		foreach(Config::get('pragmarx/ci::testers') as $name => $data)
-		{
-			$this->dataRepository->createOrUpdateTester($name, $data);
-		}
+        $this->command->line('Loading projects and suites...');
+        $this->loadProjects();
 
-		$this->dataRepository->deleteUnavailableTesters(array_keys(Config::get('pragmarx/ci::testers')));
-	}
+        $this->command->line('Loading tests...');
+        $this->loadTests();
+    }
 
-	/**
-	 * Load all projects to database.
-	 *
-	 */
-	private function loadProjects()
-	{
-		foreach(Config::get('pragmarx/ci::projects') as $name => $data)
-		{
-			$project = $this->dataRepository->createOrUpdateProject($name, $data['path'], $data['tests_path']);
+    /**
+     * Load all testers to database.
 
-			foreach($data['suites'] as $suite_name => $suite_data)
-			{
-				$this->dataRepository->createOrUpdateSuite($name, $project->id, $suite_data);
-			}
+     */
+    private function loadTesters()
+    {
+        foreach (Config::get('pragmarx/ci::testers') as $name => $data) {
+            $this->dataRepository->createOrUpdateTester($name, $data);
+        }
 
-			$this->addToWatchFolders($data['path'], $data['watch_folders']);
+        $this->dataRepository->deleteUnavailableTesters(array_keys(Config::get('pragmarx/ci::testers')));
+    }
 
-			$this->addToExclusions($data['path'], $data['exclude_folders']);
-		}
+    /**
+     * Load all projects to database.
 
-		$this->dataRepository->deleteUnavailableProjects(array_keys(Config::get('pragmarx/ci::projects')));
-	}
+     */
+    private function loadProjects()
+    {
+        foreach (Config::get('pragmarx/ci::projects') as $name => $data) {
+            $project = $this->dataRepository->createOrUpdateProject($name,
+                $data[ 'path' ], $data[ 'tests_path' ]);
 
-	/**
-	 * Load all test files to database.
-	 *
-	 */
-	private function loadTests()
-	{
-		$this->dataRepository->syncTests($this->exclusions);
-	}
+            foreach ($data[ 'suites' ] as $suite_name => $suite_data) {
+                $this->dataRepository->createOrUpdateSuite($name, $project->id,
+                    $suite_data);
+            }
 
-	/**
-	 * Add folders to the watch list.
-	 *
-	 * @param $path
-	 * @param $watch_folders
-	 */
-	private function addToWatchFolders($path, $watch_folders)
-	{
-		foreach($watch_folders as $folder)
-		{
-			$this->watchFolders[] = make_path([$path, $folder]);
-		}
-	}
+            $this->addToWatchFolders($data[ 'path' ], $data[ 'watch_folders' ]);
 
-	private function addToExclusions($path, $exclude_folders)
-	{
-		foreach($exclude_folders as $folder)
-		{
-			$this->exclusions[] = make_path([$path, $folder]);
-		}
-	}
+            $this->addToExclusions($data[ 'path' ], $data[ 'exclude_folders' ]);
+        }
 
-	private function watch()
-	{
-		$this->command->line('Booting watchers...');
+        $this->dataRepository->deleteUnavailableProjects(array_keys(Config::get('pragmarx/ci::projects')));
+    }
 
-		$me = $this;
+    /**
+     * Load all test files to database.
 
-		foreach($this->watchFolders as $folder)
-		{
-			if ( ! file_exists($folder))
-			{
-				$this->command->line("Folder {$folder} does not exists");
+     */
+    private function loadTests()
+    {
+        $this->dataRepository->syncTests($this->exclusions);
+    }
 
-				continue;
-			}
+    /**
+     * Add folders to the watch list.
+     *
+     * @param $path
+     * @param $watch_folders
+     */
+    private function addToWatchFolders($path, $watch_folders)
+    {
+        foreach ($watch_folders as $folder) {
+            $this->watchFolders[ ] = make_path([ $path, $folder ]);
+        }
+    }
 
-			$this->command->line('Watching '.$folder);
+    private function addToExclusions($path, $exclude_folders)
+    {
+        foreach ($exclude_folders as $folder) {
+            $this->exclusions[ ] = make_path([ $path, $folder ]);
+        }
+    }
 
-			$this->listeners[$folder] = $this->watcher->watch($folder);
+    private function watch()
+    {
+        $this->command->line('Booting watchers...');
 
-			$this->listeners[$folder]->anything(function($event, $resource, $path) use ($me)
-			{
-				if ( ! $me->isExcluded($path))
-				{
-					$me->fireEvent($event, $resource, $path);
-				}
-			});
-		}
+        $me = $this;
 
-		$this->watcher->start();
-	}
+        foreach ($this->watchFolders as $folder) {
+            if ( ! file_exists($folder)) {
+                $this->command->line("Folder {$folder} does not exists");
 
-	/**
-	 * Fire file modified event.
-	 *
-	 * @param $event
-	 * @param $resource
-	 * @param $path
-	 */
-	public function fireEvent($event, $resource, $path)
-	{
-		if ($event->getCode() == Event::RESOURCE_CREATED)
-		{
-			$this->loadTests();
-		}
+                continue;
+            }
 
-		$message = "File {$path} was ".$this->getEventName($event->getCode());
+            $this->command->line('Watching ' . $folder);
 
-		$this->command->drawLine($message);
+            $this->listeners[ $folder ] = $this->watcher->watch($folder);
 
-		$this->command->line($message);
+            $this->listeners[ $folder ]->anything(function (
+                $event,
+                $resource,
+                $path
+            ) use ($me) {
+                if ( ! $me->isExcluded($path)) {
+                    $me->fireEvent($event, $resource, $path);
+                }
+            });
+        }
 
-		if ($test = $this->dataRepository->isTestFile($path))
-		{
-			$this->command->line('Test added to queue');
+        $this->watcher->start();
+    }
 
-			$this->dataRepository->addTestToQueue($test);
+    /**
+     * Fire file modified event.
+     *
+     * @param $event
+     * @param $resource
+     * @param $path
+     */
+    public function fireEvent($event, $resource, $path)
+    {
+        if ($event->getCode() == Event::RESOURCE_CREATED) {
+            $this->loadTests();
+        }
 
-			return;
-		}
+        $message = "File {$path} was " . $this->getEventName($event->getCode());
 
-		if ($this->queue_test_suites($path)) {
-			return;
-		}
+        $this->command->drawLine($message);
 
-		$this->command->line('All tests added to queue');
+        $this->command->line($message);
 
-		$this->dataRepository->queueAllTests();
-	}
+        if ($test = $this->dataRepository->isTestFile($path)) {
+            $this->command->line('Test added to queue');
 
-	private function getEventName($eventCode)
-	{
-		$event = '(unknown event)';
+            $this->dataRepository->addTestToQueue($test);
 
-		switch($eventCode)
-        {
-		    case Event::RESOURCE_DELETED:
-		        $event = "deleted";
-		        break;
-		    case Event::RESOURCE_CREATED:
-			    $event = "created";
-		        break;
-		    case Event::RESOURCE_MODIFIED:
-			    $event = "modified";
-		        break;
-		}
+            return;
+        }
 
-		return $event;
-	}
+        if ($this->queueTestSuites($path)) {
+            return;
+        }
 
-	public function isExcluded($folder)
-	{
-		return $this->dataRepository->isExcluded($this->exclusions, $folder);
-	}
+        $this->command->line('All tests added to queue');
 
-	/**
-	 * @param $path
-	 * @return bool tests were queued
-	 */
-	private function queue_test_suites($path)
-	{
-		$queued = false;
-		// At this point we know a project file changed. Let's see if we
-		// can figure out which project's tests should be queued ...
-		// get all projects
-		$projects = Project::all();
+        $this->dataRepository->queueAllTests();
+    }
 
-		// Reduce the collection of projects by those whose path properties
-		// (should be only 1) are contained in the fullpath of our
-		// changed file
-		$filtered_projects = $projects->filter(function ($project) use ($path) {
-			return substr_count($path, $project->path) > 0;
-		});
+    private function getEventName($eventCode)
+    {
+        $event = '(unknown event)';
 
-		// at this point we have (hopefully only 1) project. Now we need
-		// the suite(s) associated with the project.
-		$suites = Suite::whereIn('project_id', $filtered_projects->lists('id'))
-			->get();
+        switch ($eventCode) {
+            case Event::RESOURCE_DELETED:
+                $event = "deleted";
+                break;
+            case Event::RESOURCE_CREATED:
+                $event = "created";
+                break;
+            case Event::RESOURCE_MODIFIED:
+                $event = "modified";
+                break;
+        }
 
-		foreach ($suites as $suite) {
-			$queued = true;
-			$this->command->line('Adding all tests for the ' . $suite->name . ' suite');
-			$this->dataRepository->queueTestsForSuite($suite->id);
-		}
-		return $queued;
-	}
+        return $event;
+    }
+
+    public function isExcluded($folder)
+    {
+        return $this->dataRepository->isExcluded($this->exclusions, $folder);
+    }
+
+    /**
+     * @param $path
+     *
+     * @return bool tests were queued
+     */
+    private function queueTestSuites($path)
+    {
+        $queued = false;
+
+        $suites = $this->dataRepository->getSuitesForPath($path);
+
+        foreach ($suites as $suite) {
+            $queued = true;
+            $this->command->line('Adding all tests for the ' . $suite->name . ' suite');
+            $this->dataRepository->queueTestsForSuite($suite->id);
+        }
+        return $queued;
+    }
 
 }
