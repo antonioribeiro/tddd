23b72f82e3a963c1f7fa89fd4b76c93e0fc29f0c
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-12-22 13:36:27 -0200

    Add run all and add doc blocks

--- src/Data/Repositories/Data.php
+++ src/Data/Repositories/Data.php
@@ -2,19 +2,22 @@
 
 namespace PragmaRX\Ci\Data\Repositories;
 
-use PragmaRX\Ci\Vendor\Laravel\Entities\Queue;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 use PragmaRX\Ci\Vendor\Laravel\Entities\Run;
+use PragmaRX\Ci\Vendor\Laravel\Entities\Test;
+use PragmaRX\Ci\Vendor\Laravel\Entities\Suite;
+use PragmaRX\Ci\Vendor\Laravel\Entities\Queue;
 use PragmaRX\Ci\Vendor\Laravel\Entities\Tester;
 use PragmaRX\Ci\Vendor\Laravel\Entities\Project;
-use PragmaRX\Ci\Vendor\Laravel\Entities\Suite;
-use PragmaRX\Ci\Vendor\Laravel\Entities\Test;
-use Symfony\Component\Finder\Finder;
-
-use Symfony\Component\Finder\SplFileInfo;
 use SensioLabs\AnsiConverter\AnsiToHtmlConverter;
 
 class Data {
 
+	/**
+	 * Internal constants.
+	 *
+	 */
 	const STATE_IDLE = 'idle';
 
 	const STATE_QUEUED = 'queued';
@@ -25,6 +28,12 @@ class Data {
 
 	const STATE_RUNNING = 'running';
 
+	/**
+	 * Create or update a tester.
+	 *
+	 * @param $name
+	 * @param $data
+	 */
 	public function createOrUpdateTester($name, $data)
 	{
 		Tester::updateOrCreate(
@@ -38,11 +47,27 @@ class Data {
 		);
 	}
 
+	/**
+	 * Create or update a project.
+	 *
+	 * @param $name
+	 * @param $path
+	 * @param $tests_path
+	 * @return static
+	 */
 	public function createOrUpdateProject($name, $path, $tests_path)
 	{
 		return Project::updateOrCreate(['name' => $name], ['path' => $path, 'tests_path' => $tests_path]);
 	}
 
+	/**
+	 * Create or update a suite.
+	 *
+	 * @param $name
+	 * @param $project_id
+	 * @param $suite_data
+	 * @return static
+	 */
 	public function createOrUpdateSuite($name, $project_id, $suite_data)
 	{
 		$tester = Tester::where('name', $suite_data['tester'])->first();
@@ -59,11 +84,22 @@ class Data {
 		);
 	}
 
+	/**
+	 * Get all suites.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Collection|static[]
+	 */
 	public function getSuites()
 	{
 		return Suite::all();
 	}
 
+	/**
+	 * Create or update a test.
+	 *
+	 * @param $file
+	 * @param $suite
+	 */
 	public function createOrUpdateTest($file, $suite)
 	{
 		$exists = Test::where('name', $file->getRelativePathname())
@@ -83,6 +119,11 @@ class Data {
 		}
 	}
 
+	/**
+	 * Sync all tests.
+	 *
+	 * @param $exclusions
+	 */
 	public function syncTests($exclusions)
 	{
 		foreach($this->getSuites() as $suite)
@@ -91,6 +132,12 @@ class Data {
 		}
 	}
 
+	/**
+	 * Sync all tests for a particular suite.
+	 *
+	 * @param $suite
+	 * @param $exclusions
+	 */
 	private function syncTestsForSuite($suite, $exclusions)
 	{
 		$files = $this->getAllFilesFromSuite($suite);
@@ -121,6 +168,12 @@ class Data {
 		}
 	}
 
+	/**
+	 * Get all files from a suite.
+	 *
+	 * @param $suite
+	 * @return array
+	 */
 	private function getAllFilesFromSuite($suite)
 	{
 		$files = Finder::create()->files()->in($suite->testsFullPath);
@@ -133,6 +186,12 @@ class Data {
 		return iterator_to_array($files, false);
 	}
 
+	/**
+	 * Check if a file is a test file.
+	 *
+	 * @param $path
+	 * @return \___PHPSTORM_HELPERS\static|bool|mixed
+	 */
 	public function isTestFile($path)
 	{
 		foreach(Test::all() as $test)
@@ -146,6 +205,10 @@ class Data {
 		return false;
 	}
 
+	/**
+	 * Queue all tests.
+	 *
+	 */
 	public function queueAllTests()
 	{
 		foreach(Test::all() as $test)
@@ -154,22 +217,34 @@ class Data {
 		}
 	}
 
+	/**
+	 * Queue all tests from a particular suite.
+	 *
+	 * @param $suite_id
+	 */
 	public function queueTestsForSuite($suite_id)
 	{
 		$tests = Test::where('suite_id', $suite_id)->get();
+
 		foreach($tests as $test)
 		{
 			$this->addTestToQueue($test);
 		}
 	}
 
-	public function addTestToQueue($test)
+	/**
+	 * Add a test to the queue.
+	 *
+	 * @param $test
+	 * @param bool $force
+	 */
+	public function addTestToQueue($test, $force = false)
 	{
 		if ($test->enabled && ! $this->isEnqueued($test))
 		{
 			Queue::updateOrCreate(['test_id' => $test->id]);
 
-			if ( ! in_array($test->state, [self::STATE_RUNNING, self::STATE_QUEUED]))
+			if ( $force || ! in_array($test->state, [self::STATE_RUNNING, self::STATE_QUEUED]))
 			{
 				$test->state = self::STATE_QUEUED;
 				$test->timestamps = false;
@@ -178,6 +253,10 @@ class Data {
 		}
 	}
 
+	/**
+	 * Get a test from the queue.
+	 *
+	 */
 	public function getNextTestFromQueue()
 	{
 		$query = Queue::join('tests', 'tests.id', '=', 'queue.test_id')
@@ -191,6 +270,14 @@ class Data {
 		return $queue->test;
 	}
 
+	/**
+	 * Store the test result.
+	 *
+	 * @param $test
+	 * @param $lines
+	 * @param $ok
+	 * @return mixed
+	 */
 	public function storeTestResult($test, $lines, $ok)
 	{
 		$run = Run::create([
@@ -210,6 +297,12 @@ class Data {
 		return $ok;
 	}
 
+	/**
+	 * Remove test from que run queue.
+	 *
+	 * @param $test
+	 * @return mixed
+	 */
 	private function removeTestFromQueue($test)
 	{
 		Queue::where('test_id', $test->id)->delete();
@@ -217,6 +310,11 @@ class Data {
 		return $test;
 	}
 
+	/**
+	 * Mark a test as being running.
+	 *
+	 * @param $test
+	 */
 	public function markTestAsRunning($test)
 	{
 		$test->state = self::STATE_RUNNING;
@@ -224,6 +322,11 @@ class Data {
 		$test->save();
 	}
 
+	/**
+	 * Delete unavailable testers.
+	 *
+	 * @param $testers
+	 */
 	public function deleteUnavailableTesters($testers)
 	{
 		foreach(Tester::all() as $tester)
@@ -235,6 +338,11 @@ class Data {
 		}
 	}
 
+	/**
+	 * Delete unavailable projects.
+	 *
+	 * @param $projects
+	 */
 	public function deleteUnavailableProjects($projects)
 	{
 		foreach(Project::all() as $project)
@@ -246,6 +354,14 @@ class Data {
 		}
 	}
 
+	/**
+	 * Check if a path is excluded.
+	 *
+	 * @param $exclusions
+	 * @param $path
+	 * @param string $file
+	 * @return bool
+	 */
 	public function isExcluded($exclusions, $path, $file = '')
 	{
 		if ($file)
@@ -272,6 +388,8 @@ class Data {
 	}
 
 	/**
+	 * Find a test by name and suite.
+	 *
 	 * @param $suite
 	 * @param $file
 	 * @return mixed
@@ -281,6 +399,12 @@ class Data {
 		return Test::where('name', $file->getRelativePathname())->where('suite_id', $suite->id)->first();
 	}
 
+	/**
+	 * Get all tests.
+	 *
+	 * @param null $project_id
+	 * @return array
+	 */
 	public function getTests($project_id = null)
 	{
 		$tests = [];
@@ -330,11 +454,22 @@ class Data {
 		return $tests;
 	}
 
+	/**
+	 * Get all projects.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Collection|static[]
+	 */
 	public function getProjects()
 	{
 		return Project::all();
 	}
 
+	/**
+	 * Format output log.
+	 *
+	 * @param $log
+	 * @return mixed|string
+	 */
 	private function formatLog($log)
 	{
 		if ($log)
@@ -345,6 +480,12 @@ class Data {
 		return $log;
 	}
 
+	/**
+	 * Convert output ansi chars to html.
+	 *
+	 * @param $log
+	 * @return mixed|string
+	 */
 	private function ansi2Html($log)
 	{
 		$converter = new AnsiToHtmlConverter();
@@ -360,20 +501,21 @@ class Data {
 		return $log;
 	}
 
+	/**
+	 * Enable tests.
+	 *
+	 * @param $enable
+	 * @param $project_id
+	 * @param null $test_id
+	 * @return bool
+	 */
 	public function enableTests($enable, $project_id, $test_id = null)
 	{
 		$enable = $enable === 'true';
 
-		$query = Test::select('tests.*')
-						->join('suites','suites.id', '=', 'tests.suite_id')
-						->where('suites.project_id', $project_id);
+		$tests = $this->queryTests($project_id, $test_id)->get();
 
-		if ($test_id)
-		{
-			$query->where('tests.id', $test_id);
-		}
-
-		foreach($query->get() as $test)
+		foreach($tests as $test)
 		{
 			$this->enableTest($enable, $test);
 		}
@@ -381,12 +523,25 @@ class Data {
 		return $enable;
 	}
 
-	public function runTest($test_id)
+	/**
+	 * Run a test.
+	 *
+	 * @param $test
+	 * @param bool $force
+	 */
+	public function runTest($test, $force = false)
 	{
-		$this->addTestToQueue(Test::find($test_id));
+		if ( ! $test instanceof Test)
+		{
+			$test = Test::find($test);
+		}
+
+		$this->addTestToQueue($test, $force);
 	}
 
 	/**
+	 * Enable a test.
+	 *
 	 * @param $enable
 	 * @param $test
 	 */
@@ -409,6 +564,14 @@ class Data {
 		}
 	}
 
+	/**
+	 * Get the test output.
+	 *
+	 * @param $test
+	 * @param $outputFolder
+	 * @param $extension
+	 * @return bool|mixed|null|string
+	 */
 	private function getOutput($test, $outputFolder, $extension)
 	{
 		if ( ! $outputFolder)
@@ -428,6 +591,12 @@ class Data {
 		return null;
 	}
 
+	/**
+	 * Encode a image or html for database storage.
+	 *
+	 * @param $file
+	 * @return bool|mixed|string
+	 */
 	private function encodeFile($file)
 	{
 		$type = pathinfo($file, PATHINFO_EXTENSION);
@@ -442,6 +611,12 @@ class Data {
 		return 'data:image/' . $type . ';base64,' . base64_encode($data);
 	}
 
+	/**
+	 * Is the test in the queue?
+	 *
+	 * @param $test
+	 * @return bool
+	 */
 	private function isEnqueued($test)
 	{
 		return
@@ -450,6 +625,12 @@ class Data {
 			Queue::where('test_id', $test->id)->first();
 	}
 
+	/**
+	 * Get all suites for a path.
+	 *
+	 * @param $path
+	 * @return mixed
+	 */
 	public function getSuitesForPath($path)
 	{
 		$projects = $this->getProjects();
@@ -466,6 +647,49 @@ class Data {
 		// the suite(s) associated with the project.
 		return Suite::whereIn('project_id', $filtered_projects->lists('id'))
 				->get();
+	}
+
+	/**
+	 * Run all tests or projects tests.
+	 *
+	 * @param null $project_id
+	 */
+	public function runAll($project_id = null)
+	{
+		$tests = $this->queryTests($project_id)->get();
+
+		foreach($tests as $test)
+		{
+			$this->enableTest(true, $test);
+
+			// Force test to the queue
+			$this->runTest($test, true);
+		}
+	}
+
+	/**
+	 * Query tests.
+	 *
+	 * @param $project_id
+	 * @param $test_id
+	 * @return mixed
+	 */
+	private function queryTests($project_id = null, $test_id = null)
+	{
+		$query = Test::select('tests.*')
+					->join('suites', 'suites.id', '=', 'tests.suite_id');
+
+		if ($project_id)
+		{
+			$query->where('suites.project_id', $project_id);
+		}
+
+		if ($test_id)
+		{
+			$query->where('tests.id', $test_id);
+		}
+
+		return $query;
 	}
 
 }
