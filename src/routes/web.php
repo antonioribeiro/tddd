2c795e8e2a3bee0101b061eb4a08a9ade72b056d
Simon Ellensohn <simon.ellensohn@towa.at>
2017-10-09 16:17:44 +0200

    remove duplicate route definitions

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,20 +14,6 @@ Route::group(['prefix' => '/tests'], function() {
     Route::get('/{project_id?}', ['as' => 'tests-watcher.tests.project', 'uses' => 'DashboardController@allTests']);
 });
 
-Route::group(['prefix' => '/tests'], function() {
-    Route::get('/run/all/{project_id}', ['as' => 'tests-watcher.tests.run.all', 'uses' => 'DashboardController@runAll']);
-
-    Route::get('/reset/{project_id}', ['as' => 'tests-watcher.tests.reset', 'uses' => 'DashboardController@reset']);
-
-    Route::get('/run/{test_id?}', ['as' => 'tests-watcher.tests.run', 'uses' => 'DashboardController@runTest']);
-
-    Route::get('/enable/{enable}/{project_id}/{test_id?}', ['as' => 'tests-watcher.tests.enable', 'uses' => 'DashboardController@enableTests']);
-
-    Route::get('/notify/{project_id}', ['as' => 'tests-watcher.tests.notify', 'uses' => 'DashboardController@notify']);
-
-    Route::get('/{project_id?}', ['as' => 'tests-watcher.tests.project', 'uses' => 'DashboardController@allTests']);
-});
-
 Route::get('/projects', ['as' => 'tests-watcher.projects', 'uses' => 'DashboardController@allProjects']);
 
 Route::get('/dashboard', ['as' => 'tests-watcher.dashboard', 'uses' => 'DashboardController@index']);
